be94740a56b9e86aec788a9788fe02c39c126efd
carneirofc <claudiofcarneiro@hotmail.com>
2020-09-10 14:58:09 -0300

    Removed EPPS IOCS

--- iocBoot/iocCONSProcServ/st.cmd
+++ iocBoot/iocCONSProcServ/st.cmd
@@ -26,18 +26,6 @@ drvAsynIPPortConfigure("P11",  "10.128.124.161:20200", 100, 0, 0) # ProcCtrl:ToS
 drvAsynIPPortConfigure("P12",  "10.128.124.162:20200", 100, 0, 0) # ProcCtrl:ToSI-SSA02
 drvAsynIPPortConfigure("P13",  "10.128.124.165:20200", 100, 0, 0) # ProcCtrl:RA-RaSIA01:RF-LLRFCalSys
 drvAsynIPPortConfigure("P14",  "10.128.124.166:20200", 100, 0, 0) # ProcCtrl:RA-RaSIA01:RF-CavPlDrivers
-drvAsynIPPortConfigure("P15",  "10.128.255.3:21001", 100, 0, 0) # ProcCtrl:TB-04:PU-InjSept
-drvAsynIPPortConfigure("P16",  "10.128.255.3:21002", 100, 0, 0) # ProcCtrl:BO-01D:PU-InjKckr
-drvAsynIPPortConfigure("P17",  "10.128.255.3:21003", 100, 0, 0) # ProcCtrl:TS-04:PU-InjSeptG-1
-drvAsynIPPortConfigure("P18",  "10.128.255.3:21004", 100, 0, 0) # ProcCtrl:TS-04:PU-InjSeptG-2
-drvAsynIPPortConfigure("P19",  "10.128.255.3:21005", 100, 0, 0) # ProcCtrl:TS-04:PU-InjSeptF
-drvAsynIPPortConfigure("P20",  "10.128.255.3:21006", 100, 0, 0) # ProcCtrl:SI-01SA:PU-PingH
-drvAsynIPPortConfigure("P21",  "10.128.255.3:21007", 100, 0, 0) # ProcCtrl:SI-01SA:PU-InjDpKckr
-drvAsynIPPortConfigure("P22",  "10.128.255.3:21010", 100, 0, 0) # ProcCtrl:SI-01SA:PU-InjNLKckr
-drvAsynIPPortConfigure("P23",  "10.128.255.3:21011", 100, 0, 0) # ProcCtrl:BO-48D:PU-EjeKckr
-drvAsynIPPortConfigure("P24",  "10.128.255.3:21012", 100, 0, 0) # ProcCtrl:TS-01:PU-EjeSeptF
-drvAsynIPPortConfigure("P25",  "10.128.255.3:21013", 100, 0, 0) # ProcCtrl:TS-01:PU-EjeSeptG
-drvAsynIPPortConfigure("P26",  "10.128.255.3:21014", 100, 0, 0) # ProcCtrl:SI-19C4:PU-PingV
 
 dbLoadRecords("db/procServControl.db","P=ProcCtrl:BR-RF-DLLRF-01,PORT=P0,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:BR-RF-DLLRF-01")
 dbLoadRecords("db/procServControl.db","P=ProcCtrl:ILK-Booster,PORT=P1,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:ILK-Booster")
@@ -54,18 +42,6 @@ dbLoadRecords("db/procServControl.db","P=ProcCtrl:ToSI-SSA01,PORT=P11,SHOWOUT=1,
 dbLoadRecords("db/procServControl.db","P=ProcCtrl:ToSI-SSA02,PORT=P12,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:ToSI-SSA02")
 dbLoadRecords("db/procServControl.db","P=ProcCtrl:RA-RaSIA01:RF-LLRFCalSys,PORT=P13,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:RA-RaSIA01:RF-LLRFCalSys")
 dbLoadRecords("db/procServControl.db","P=ProcCtrl:RA-RaSIA01:RF-CavPlDrivers,PORT=P14,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:RA-RaSIA01:RF-CavPlDrivers")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:TB-04:PU-InjSept,PORT=P15,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:TB-04:PU-InjSept")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:BO-01D:PU-InjKckr,PORT=P16,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:BO-01D:PU-InjKckr")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:TS-04:PU-InjSeptG-1,PORT=P17,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:TS-04:PU-InjSeptG-1")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:TS-04:PU-InjSeptG-2,PORT=P18,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:TS-04:PU-InjSeptG-2")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:TS-04:PU-InjSeptF,PORT=P19,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:TS-04:PU-InjSeptF")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:SI-01SA:PU-PingH,PORT=P20,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:SI-01SA:PU-PingH")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:SI-01SA:PU-InjDpKckr,PORT=P21,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:SI-01SA:PU-InjDpKckr")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:SI-01SA:PU-InjNLKckr,PORT=P22,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:SI-01SA:PU-InjNLKckr")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:BO-48D:PU-EjeKckr,PORT=P23,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:BO-48D:PU-EjeKckr")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:TS-01:PU-EjeSeptF,PORT=P24,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:TS-01:PU-EjeSeptF")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:TS-01:PU-EjeSeptG,PORT=P25,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:TS-01:PU-EjeSeptG")
-dbLoadRecords("db/procServControl.db","P=ProcCtrl:SI-19C4:PU-PingV,PORT=P26,SHOWOUT=1,MANUALSTART=,NAME=ProcCtrl:SI-19C4:PU-PingV")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
@@ -87,15 +63,3 @@ seq(procServControl,"P=ProcCtrl:ToSI-SSA01")
 seq(procServControl,"P=ProcCtrl:ToSI-SSA02")
 seq(procServControl,"P=ProcCtrl:RA-RaSIA01:RF-LLRFCalSys")
 seq(procServControl,"P=ProcCtrl:RA-RaSIA01:RF-CavPlDrivers")
-seq(procServControl,"P=ProcCtrl:TB-04:PU-InjSept")
-seq(procServControl,"P=ProcCtrl:BO-01D:PU-InjKckr")
-seq(procServControl,"P=ProcCtrl:TS-04:PU-InjSeptG-1")
-seq(procServControl,"P=ProcCtrl:TS-04:PU-InjSeptG-2")
-seq(procServControl,"P=ProcCtrl:TS-04:PU-InjSeptF")
-seq(procServControl,"P=ProcCtrl:SI-01SA:PU-PingH")
-seq(procServControl,"P=ProcCtrl:SI-01SA:PU-InjDpKckr")
-seq(procServControl,"P=ProcCtrl:SI-01SA:PU-InjNLKckr")
-seq(procServControl,"P=ProcCtrl:BO-48D:PU-EjeKckr")
-seq(procServControl,"P=ProcCtrl:TS-01:PU-EjeSeptF")
-seq(procServControl,"P=ProcCtrl:TS-01:PU-EjeSeptG")
-seq(procServControl,"P=ProcCtrl:SI-19C4:PU-PingV")
